be750dd96a394da0f96794c2aa53f57d96ffffac
jumatemory <juma.temory@gmail.com>
2020-04-11 02:26:17 -0400

    first commit

---
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -0,0 +1,9 @@
+package com.vytrack.runners;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/Login.feature")
+public class CucumberRunner {
+}
